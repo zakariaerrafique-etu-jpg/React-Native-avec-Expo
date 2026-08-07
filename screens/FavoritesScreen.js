632afe8867ccf8408f53998e5f145a94af850bf6
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:39:00 +0200

    Create FavoritesScreen to show favorite items
    Implement FavoritesScreen to display a list of favorite items using FlatList.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,1 +1,85 @@
 
+import React,{useState} from "react";
+
+import {
+View,
+Text,
+FlatList,
+StyleSheet
+} from "react-native";
+
+
+export default function FavoritesScreen(){
+
+
+const [favorites]=useState([
+
+{
+id:1,
+name:"React Native Course"
+},
+
+{
+id:2,
+name:"Mobile Development"
+}
+
+]);
+
+
+
+return(
+
+<View style={styles.container}>
+
+
+<Text style={styles.title}>
+Favorites
+</Text>
+
+
+<FlatList
+
+data={favorites}
+
+keyExtractor={
+item=>item.id.toString()
+}
+
+
+renderItem={({item})=>(
+
+<Text style={styles.item}>
+⭐ {item.name}
+</Text>
+
+)}
+
+
+/>
+
+
+</View>
+
+)
+
+}
+
+
+
+const styles=StyleSheet.create({
+
+container:{
+padding:20
+},
+
+title:{
+fontSize:30
+},
+
+item:{
+padding:15,
+fontSize:18
+}
+
+});
