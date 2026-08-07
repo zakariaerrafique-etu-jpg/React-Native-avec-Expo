2cec8ede1ba57ffbc01bb6b9c40cf5be3b236ad2
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:37:54 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,110 @@
+import React,{useEffect,useState} from "react";
 
-import React,{useEffect,useState} from 'react';
-import {View,Text,FlatList} from 'react-native';
+import {
+View,
+Text,
+FlatList,
+TouchableOpacity,
+StyleSheet
+} from "react-native";
 
-export default function HomeScreen(){
 
-const[data,setData]=useState([]);
+export default function HomeScreen({navigation}){
+
+
+const [articles,setArticles]=useState([]);
+
+
 
 useEffect(()=>{
 
-fetch("https://jsonplaceholder.typicode.com/posts")
 
-.then(r=>r.json())
+fetch(
+"https://jsonplaceholder.typicode.com/posts"
+)
 
-.then(setData);
+.then(response=>response.json())
+
+.then(data=>setArticles(data.slice(0,10)));
+
 
 },[]);
 
+
+
 return(
+
+<View style={styles.container}>
+
+
+<Text style={styles.header}>
+📚 Learner App
+</Text>
+
 
 <FlatList
 
-data={data}
+data={articles}
 
-keyExtractor={item=>item.id.toString()}
-
-renderItem={({item})=>
-
-<Text>{item.title}</Text>
-
+keyExtractor={
+item=>item.id.toString()
 }
+
+
+renderItem={({item})=>(
+
+
+<TouchableOpacity
+
+style={styles.card}
+
+onPress={()=>navigation.navigate(
+"Detail",
+{article:item}
+)}
+
+>
+
+
+<Text>
+{item.title}
+</Text>
+
+
+</TouchableOpacity>
+
+
+)}
 
 />
 
-);
+
+</View>
+
+)
 
 }
+
+
+
+const styles=StyleSheet.create({
+
+container:{
+flex:1,
+padding:15
+},
+
+header:{
+fontSize:25,
+fontWeight:"bold",
+marginBottom:20
+},
+
+card:{
+padding:20,
+borderWidth:1,
+marginBottom:10,
+borderRadius:10
+}
+
+});
