27c92a52eaeb43fea7298d65077e5376ff1cdd95
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:33:08 +0200

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,1 +1,46 @@
+import React,{useState} from 'react';
+import {View,TextInput,Button,Alert} from 'react-native';
 
+export default function LoginScreen(){
+
+const[email,setEmail]=useState("");
+const[password,setPassword]=useState("");
+
+const login=()=>{
+
+if(email===""||password===""){
+Alert.alert("Login Error");
+return;
+}
+
+Alert.alert("Welcome");
+
+}
+
+return(
+
+<View>
+
+<TextInput
+placeholder="Email"
+value={email}
+onChangeText={setEmail}
+/>
+
+<TextInput
+placeholder="Password"
+secureTextEntry
+value={password}
+onChangeText={setPassword}
+/>
+
+<Button
+title="Login"
+onPress={login}
+/>
+
+</View>
+
+)
+
+}
