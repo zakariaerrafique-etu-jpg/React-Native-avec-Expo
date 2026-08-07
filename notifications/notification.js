f1c5c5efb0027eaadd22b7977bb5476f0515b6ba
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:35:28 +0200

    Update notification.js

--- notifications/notification.js
+++ notifications/notification.js
@@ -1,1 +1,20 @@
 
+import * as Notifications from 'expo-notifications';
+
+export async function sendNotification(){
+
+await Notifications.scheduleNotificationAsync({
+
+content:{
+
+title:"Learner App",
+
+body:"Learning reminder"
+
+},
+
+trigger:{seconds:5}
+
+});
+
+}
