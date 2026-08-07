6e910d21309ed582dff442ad118929d538f64516
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:38:28 +0200

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,23 +1,68 @@
+import React from "react";
 
-import React from 'react';
-import {View,Text} from 'react-native';
+import {
+View,
+Text,
+StyleSheet
+} from "react-native";
+
 
 export default function SettingsScreen(){
 
+
 return(
 
-<View>
+<View style={styles.container}>
 
-<Text>Notifications</Text>
 
-<Text>Dark Mode</Text>
+<Text style={styles.title}>
+Settings
+</Text>
 
-<Text>Language</Text>
 
-<Text>Logout</Text>
+<Text style={styles.item}>
+🔔 Notifications
+</Text>
+
+
+<Text style={styles.item}>
+🌙 Dark Mode
+</Text>
+
+
+<Text style={styles.item}>
+🌐 Language
+</Text>
+
+
+<Text style={styles.item}>
+Logout
+</Text>
+
 
 </View>
 
 )
 
 }
+
+
+
+const styles=StyleSheet.create({
+
+container:{
+padding:20
+},
+
+title:{
+fontSize:30,
+marginBottom:30
+},
+
+item:{
+fontSize:18,
+padding:15,
+borderBottomWidth:1
+}
+
+});
