33ae5d9f9422d9ad4c919b63b75dc165b06d8dec
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:33:34 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,1 +1,37 @@
 
+import React,{useEffect,useState} from 'react';
+import {View,Text,FlatList} from 'react-native';
+
+export default function HomeScreen(){
+
+const[data,setData]=useState([]);
+
+useEffect(()=>{
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+
+.then(r=>r.json())
+
+.then(setData);
+
+},[]);
+
+return(
+
+<FlatList
+
+data={data}
+
+keyExtractor={item=>item.id.toString()}
+
+renderItem={({item})=>
+
+<Text>{item.title}</Text>
+
+}
+
+/>
+
+);
+
+}
