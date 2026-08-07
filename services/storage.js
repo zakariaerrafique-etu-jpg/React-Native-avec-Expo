7fd1e031574e4e949295d950c4606fcd5bb92b6f
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:34:31 +0200

    Add AsyncStorage functions for saving and loading data

--- services/storage.js
+++ services/storage.js
@@ -1,1 +1,16 @@
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const saveData=async(data)=>{
+
+await AsyncStorage.setItem("articles",JSON.stringify(data));
+
+}
+
+export const loadData=async()=>{
+
+const value=await AsyncStorage.getItem("articles");
+
+return JSON.parse(value);
+
+}
