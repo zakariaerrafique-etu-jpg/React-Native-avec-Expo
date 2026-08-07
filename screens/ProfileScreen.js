63c134114302210097a64ea357d9d7590a805ae7
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:38:46 +0200

    Update ProfileScreen.js

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,1 +1,54 @@
 
+import React from "react";
+
+import {
+View,
+Text,
+StyleSheet
+} from "react-native";
+
+
+export default function ProfileScreen(){
+
+
+return(
+
+<View style={styles.container}>
+
+
+<Text style={styles.title}>
+Profile
+</Text>
+
+
+<Text>
+Username : Student
+</Text>
+
+
+<Text>
+Email : student@gmail.com
+</Text>
+
+
+</View>
+
+
+)
+
+}
+
+
+
+const styles=StyleSheet.create({
+
+container:{
+padding:20
+},
+
+title:{
+fontSize:30,
+marginBottom:20
+}
+
+});
