956aea0ed73e04f8589bbc5c9fe423733af9828c
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:35:07 +0200

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,1 +1,23 @@
 
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function SettingsScreen(){
+
+return(
+
+<View>
+
+<Text>Notifications</Text>
+
+<Text>Dark Mode</Text>
+
+<Text>Language</Text>
+
+<Text>Logout</Text>
+
+</View>
+
+)
+
+}
