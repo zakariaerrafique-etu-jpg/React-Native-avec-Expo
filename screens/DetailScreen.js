26fa32766b77251e239374c5c831cddd36f96c1d
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:34:06 +0200

    Add DetailScreen component to display article details

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function DetailScreen({route}){
+
+const{article}=route.params;
+
+return(
+
+<View>
+
+<Text>{article.title}</Text>
+
+<Text>{article.body}</Text>
+
+</View>
+
+)
+
+}
