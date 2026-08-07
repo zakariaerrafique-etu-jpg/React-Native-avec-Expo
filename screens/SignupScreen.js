2806f8c91f163afe0d67a4e8ca2d2c53a7c097b9
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-07 22:31:38 +0200

    Add SignupScreen component with form validation

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,1 +1,53 @@
 
+import React,{useState} from 'react';
+import {View,TextInput,Button,Alert} from 'react-native';
+
+export default function SignupScreen(){
+
+const[name,setName]=useState("");
+const[email,setEmail]=useState("");
+const[password,setPassword]=useState("");
+
+const signup=()=>{
+
+if(name===""||email===""||password===""){
+Alert.alert("Error","Please fill all fields");
+return;
+}
+
+Alert.alert("Success","Account Created");
+}
+
+return(
+
+<View>
+
+<TextInput
+placeholder="Username"
+value={name}
+onChangeText={setName}
+/>
+
+<TextInput
+placeholder="Email"
+value={email}
+onChangeText={setEmail}
+/>
+
+<TextInput
+placeholder="Password"
+secureTextEntry
+value={password}
+onChangeText={setPassword}
+/>
+
+<Button
+title="Sign Up"
+onPress={signup}
+/>
+
+</View>
+
+)
+
+}
